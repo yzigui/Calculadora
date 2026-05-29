6988fc026422ca1c6660b1f174be429c6c84197d
yzigui <yasmimguimaraes883@gmail.com>
2026-05-29 10:45:38 +0100

    Update main.py
    Adicionada entrada de dados e resultado

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
     sinalizando que o cálculo não pôde ser realizado.
     """
     result = float("nan")
+
     if operador == '+':
         result = num1 + num2
+    elif operador == '-':
+        result = num1 - num2
+    elif operador == '*':
+        result = num1 * num2
+    elif operador == '/':
+        result = num1 / num2
+    elif operador == '**':
+        result = num1 ** num2
+    elif operador == '%':
+        result = num1 % num2
 
     return result
 
